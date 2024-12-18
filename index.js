87d91b4c22f17fb6f179226c26bcca94680b9d86
raselworshop <raselworshop@gmail.com>
2024-12-18 20:41:21 +0800

    added the ema-john server

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5hy3n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,11 +29,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db('emaJohnDB').collection('products');
+    const productCollection = client.db('amzone-products').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = req.query.page;
+      const size = req.query.size;
+      console.log('pagination query', page, size)
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(parseInt(size))
+        .toArray();
         res.send(result);
+    })
+    app.post('/productByIds', async (req, res) => {
+      const ids = req.body;
+      const idsWithObjectid = ids.map(id=> new ObjectId(id))
+      const query = {
+        _id:{
+          $in: idsWithObjectid
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      console.log(idsWithObjectid)
+      res.send(result)
+    })
+
+    app.get('/products/products-count', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({count})
     })
 
     // Send a ping to confirm a successful connection
